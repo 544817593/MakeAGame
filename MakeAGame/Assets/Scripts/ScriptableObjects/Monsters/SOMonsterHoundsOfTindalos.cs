fff1a61f9cf90d1c78c8b042d17c04f028852cf3
JINXL-2020 <2506953527@qq.com>
2023-05-27 13:29:10 +0800

    敌人动画
    妖鬼、星之精、猎犬、冷蛛

--- MakeAGame/Assets/Scripts/ScriptableObjects/Monsters/SOMonsterHoundsOfTindalos.cs
+++ MakeAGame/Assets/Scripts/ScriptableObjects/Monsters/SOMonsterHoundsOfTindalos.cs
@@ -5,5 +5,10 @@ using UnityEngine;
 [CreateAssetMenu (fileName = "HoundsOfTindalos", menuName = "ScriptableObjects/Pieces/Monsters/HoundsOfTindalos")]
 public class SOMonsterHoundsOfTindalos : SOMonsterBase
 {
+    public GameObject anim;
 
+    public override GameObject GetChildAnim()
+    {
+        return anim;
+    }
 }
