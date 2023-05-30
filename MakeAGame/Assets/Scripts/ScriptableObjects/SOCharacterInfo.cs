fbd5355ac2cbad721ab7d5909dc96d04a9dcbac3
xiaoxichen <xiaoxi.chen@centurygame.com>
2023-05-30 21:05:27 +0800

    SOCharacterInfo和SOFeature增加部分属性

--- MakeAGame/Assets/Scripts/ScriptableObjects/SOCharacterInfo.cs
+++ MakeAGame/Assets/Scripts/ScriptableObjects/SOCharacterInfo.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Game
@@ -9,28 +11,60 @@ namespace Game
         public int characterID; // 角色id
         public string characterName;    // 角色名字
         
+        [FoldoutGroup("Card Information", true)]
         public int rarity;  // 稀有度
+        [FoldoutGroup("Card Information")]
         public int sanCost; // 精神值消耗
+        [FoldoutGroup("Card Information")] [TextArea(1, 2)]
         public string deathFuncDescription; // 死面描述
+        [FoldoutGroup("Card Information")]
         public Sprite cardSprite;   // 卡面图片
-
+        [FoldoutGroup("Card Information")]
         public int hp;  // 血量
+        [FoldoutGroup("Card Information")]
         public int attack;  // 攻击力
+        [FoldoutGroup("Card Information")]
         public float moveSpd; // 行动速度
+        [FoldoutGroup("Card Information")]
         public int defend;  // 防御力
 
+        [FoldoutGroup("Features", true)]
         public List<SOFeature> features;  // 特性
+        [FoldoutGroup("Features")]
         public SOFeature specialFeature;  // 特殊属性
         
+        [FoldoutGroup("Piece Information", true)]
         public Sprite pieceSprite;  // 棋子图片
+        [FoldoutGroup("Piece Information")]
         public int width;   // 棋子占地宽
+        [FoldoutGroup("Piece Information")]
         public int height;  // 棋子占地长
+        [FoldoutGroup("Piece Information")]
         public DirEnum[] moveDirections;    // 移动方向
-
+        [FoldoutGroup("Piece Information")]
         public float attackSpd;   // 攻速
+        [FoldoutGroup("Piece Information")]
         public float accracy;   // 命中率
+        [FoldoutGroup("Piece Information")]
         public int attackRange; // 攻击范围
-
+        [FoldoutGroup("Piece Information")]
         public float life; // 寿命
+
+        [FoldoutGroup("Player Stat Bonus", true)]
+        public PlayerBonus sanCostBonus;
+        [FoldoutGroup("Player Stat Bonus")]
+        public PlayerBonus hpBonus;
+        [FoldoutGroup("Player Stat Bonus")]
+        public PlayerBonus atkBonus;
+        [FoldoutGroup("Player Stat Bonus")]
+        public PlayerBonus atkSpdBonus;
+        
+
+        [Serializable]
+        public class PlayerBonus
+        {
+            public PlayerStatsEnum stat;
+            public float multiple;
+        }
     }
 }
