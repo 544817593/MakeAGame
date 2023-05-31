8f664b8c689916c23c123840156da2a2019a39f6
xiaoxichen <xiaoxi.chen@centurygame.com>
2023-05-31 17:24:54 +0800

    失眠症 GetLastSpawnedFriend返回空的处理

--- MakeAGame/Assets/Scripts/QFViewController/ViewCard.cs
+++ MakeAGame/Assets/Scripts/QFViewController/ViewCard.cs
@@ -155,7 +155,8 @@ namespace Game
         {
             // 检测通知的是不是自己
             if (e.viewCard != this) return;
-            if (e.viewCard.card.charaID == this.GetSystem<IPieceSystem>().GetLastSpawnedFriend(true).card.charaID &&
+            if (this.GetSystem<IPieceSystem>().GetLastSpawnedFriend(true) != null &&
+                e.viewCard.card.charaID == this.GetSystem<IPieceSystem>().GetLastSpawnedFriend(true).card.charaID &&
                 e.viewCard.card.HasFeature("失眠症"))
             {
                 Debug.Log("失眠症，无法连续放置");
