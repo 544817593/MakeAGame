e0fe93784cc8576e913f4e10db6ece23e32ade19
xiaoxichen <xiaoxi.chen@centurygame.com>
2023-06-09 11:01:58 +0800

    gen_code_json.bat 修正

--- MakeAGame/LubanTool/gen_code_json.bat
+++ MakeAGame/LubanTool/gen_code_json.bat
@@ -6,8 +6,8 @@ set CONF_ROOT=%WORKSPACE%\LubanTool\GameConfig
 %GEN_CLIENT% -j cfg --^
  -d %CONF_ROOT%\Defines\__root__.xml ^
  --input_data_dir %CONF_ROOT%\Datas ^
- --output_code_dir %CONF_ROOT%\Assets\Scripts\LubanGen ^
- --output_data_dir %CONF_ROOT%\Assets\Resources\Json ^
+ --output_code_dir %WORKSPACE%\Assets\Scripts\LubanGen ^
+ --output_data_dir %WORKSPACE%\Assets\Resources\Json ^
  --gen_types code_cs_unity_json,data_json ^
  -s all 
 
