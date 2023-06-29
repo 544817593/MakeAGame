f2190e2b73d9fac181feeb0764df91538d1c1938
xiaoxichen <xiaoxi.chen@centurygame.com>
2023-06-30 01:23:07 +0800

    回退对话ui发送SelectMapEndCommand的修改

--- MakeAGame/Assets/Scripts/UIDialogue/DialoguePanel.cs
+++ MakeAGame/Assets/Scripts/UIDialogue/DialoguePanel.cs
@@ -24,12 +24,12 @@ namespace DialogueUI
 		
 		protected override void OnOpen(IUIData uiData = null)
 		{
-			GameEntry.Interface.SendCommand<SelectMapEndCommand>(new SelectMapEndCommand(null, true));
+			// GameEntry.Interface.SendCommand<SelectMapEndCommand>(new SelectMapEndCommand(null, true));
 		}
 		
 		protected override void OnShow()
 		{
-			GameEntry.Interface.SendCommand<SelectMapEndCommand>(new SelectMapEndCommand(null, true));
+			// GameEntry.Interface.SendCommand<SelectMapEndCommand>(new SelectMapEndCommand(null, true));
 		}
 		
 		protected override void OnHide()
